f54c43f4a36d02aeee8ac2533fc71002b84242e9
Andrey Dmitriev <admit8490@gmail.com>
2022-09-16 16:21:19 +0200

    initial commit

---
+++ 4kyu/The-observed-PIN.js
@@ -0,0 +1,14 @@
+// TASK LINK >>> https://www.codewars.com/kata/5263c6999e0f40dee200059d/train/javascript
+
+// look up
+
+const lock = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [undefined, 0, undefined]
+]
+
+function getPINs(observed) {
+  // TODO: This is your job, detective!
+}
