0519ea68c11de1e3bbacb3eca246d1211317bfd8
Andrey Dmitriev <admit8490@gmail.com>
2022-09-18 17:52:23 +0200

    string-mix solved

--- 4kyu/strings-mix.js
+++ 4kyu/strings-mix.js
@@ -16,21 +16,14 @@ const mapBuilder = (str) => {
   return lettersMap;
 }
 
-const notLessThanOne = (map1, map2, key) => {
-  const [val1, val2] = [map1[key], map2[key]];  
-  if (val1 === 1 && val2 === 1) {
-    return false;
-  }
-  if ((val1 === undefined || val2 === undefined)
-      && (val1 === 1 || val2 === 1)) {
-    return false;
-  }
-  return true;
+const moreThanOne = (map1, map2, key) => {
+  return [map1[key], map2[key]]
+    .some(el => (el !==1) && (el !== undefined));
 }
 
 const prefixAndSequenceSource = (map1, map2, key) => {  
-  const [val1, val2] = [map1[key], map2[key]];
-  const max = Math.max(val1, val2);
+  const [val1, val2] = [map1[key], map2[key]].map(el => el === undefined ? 0 : el);
+  const max = Math.max(val1, val2);  
   return val1 === val2 ? ['=:', map1]
     : val1 === max
     ? ['1:', map1]
@@ -55,9 +48,9 @@ function mix(s1, s2) {
   const map2 = mapBuilder(cleaner(s2));
   const keys = new Set(Object.keys(map1).concat(Object.keys(map2)));
   const resultArray = [];
-  keys.forEach(key => {
-    if (notLessThanOne(map1, map2, key)) {
-      const [prefix, sequenceSource] = prefixAndSequenceSource(map1, map2, key);
+  keys.forEach(key => {    
+    if (moreThanOne(map1, map2, key)) {
+      const [prefix, sequenceSource] = prefixAndSequenceSource(map1, map2, key);      
       resultArray.push(prefix + key.repeat(sequenceSource[key]));
     }
   })  
@@ -66,7 +59,7 @@ function mix(s1, s2) {
 
 const simpleTest = ["looping is fun but dangerous", "less dangerous than coding", "1:ooo/1:uuu/2:sss/=:nnn/1:ii/2:aa/2:dd/2:ee/=:gg"];
 console.log('Simple Test')
-console.log('EXPECTED  ', simpleTest[2])
-console.log('RESULT  ', mix(simpleTest[0], simpleTest[1]))
-console.log('beep,  ', simpleTest[2]);
-console.log('boop,  ',mix(simpleTest[0], simpleTest[1]));
+console.log('EXPECTED  ', "1:aaa/1:nnn/1:gg/2:ee/2:ff/2:ii/2:oo/2:rr/2:ss/2:tt")
+console.log('RESULT  ', mix(" In many languages", " there's a pair of functions"))
+console.log('beep,  ', "1:aaa/1:nnn/1:gg/2:ee/2:ff/2:ii/2:oo/2:rr/2:ss/2:tt");
+console.log('boop,  ',mix(" In many languages", " there's a pair of functions"));
