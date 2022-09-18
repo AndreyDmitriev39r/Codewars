301c73d02722939ff84bde41231f49cc7140b598
Andrey Dmitriev <admit8490@gmail.com>
2022-09-18 19:07:13 +0200

    range extraction boilerplate

---
+++ 4kyu/range-extraction.js
@@ -0,0 +1,13 @@
+// TASK LINK >>> https://www.codewars.com/kata/51ba717bb08c1cd60f00002f/train/javascript
+
+function solution(list){
+  let result = '';
+  let [start, end] = [0, 0];
+  return result;
+ }
+
+//  TESTS
+
+console.log(solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]))
+
+console.log("-6,-3-1,3-5,7-11,14,15,17-20")
