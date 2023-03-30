7edb32e9a649f24cdc238745762f0603b421f51d
Andrey Dmitriev <admit8490@gmail.com>
2023-03-30 14:53:11 +0200

    find n-th digit inf sum - tests added

--- 3kyu/find-nth-digit-in-a-infinite-addition-result.js
+++ 3kyu/find-nth-digit-in-a-infinite-addition-result.js
@@ -1,45 +1,5 @@
 // TASK LINK >>> https://www.codewars.com/kata/59f6e1af3640ce12510000ad
 
-const range = (start, stop, step) =>
-  Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step);
-
-// generate n-length sequence of natural nums
-
-const genNumSequence = (n) => {
-  let result = "";
-  let num = 1;
-  while (result.length < n) {
-    result += num;
-    num++
-  }
-  return Number(result.substring(0, n));
-}
-
-// for (let i = 11; i < 19; i++) {
-//   console.log(`CASE n=${i}`);
-//   console.log(`length of result is ${genNumSequence(i).length}`);
-//   console.log(`result is ${genNumSequence(i)}`);
-// }
-
-
-// generate n-length sequence of squares
-
-const genSquareSequence = (n) => {
-  let result = "";
-  let num = 1;
-  while(result.length < n) {
-    result += num ** 2;
-    num ++;
-  }
-  return Number(result.substring(0, n));
-}
-
-// for (let i = 1; i < 10; i++) {
-//   console.log(`CASE n=${i}`);
-//   console.log(`length of result is ${genSquareSequence(i).length}`);
-//   console.log(`result is ${genSquareSequence(i)}`);
-// }
-
 // 272619325597593231536305887388...
 
 const genSequence = (n, isSquare = false) => {
@@ -49,19 +9,32 @@ const genSequence = (n, isSquare = false) => {
     result += isSquare ? num ** 2 : num;
     num ++;
   }
-  return result.substring(0, n + 1).split('').map(el => Number(el));
+  return result.substring(0, n + 1);
 }
 
 const findDigit = (n) => {  
   const [naturals, squares] = [genSequence(n + 1, false), genSequence(n + 1, true)];  
-  let result = naturals[n] + squares[n];
-  result += naturals[n + 1] + squares[n + 1] >= 10 ? 1 : 0;
+  let result = Number(naturals[n]) + Number(squares[n]);
+  result += Number(naturals[n + 1]) + Number(squares[n + 1]) >= 10 ? 1 : 0;
   return result < 10 ? result : result - 10;
 }
 
-for (let i = 0; i < 10; i++) {
-  console.log(`CASE n === ${i}`);
-  console.log(findDigit(i));
+// test cases
+
+const sum = '272619325597593231536305887388';
+let expected; 
+let result;
+for (let i = 0; i < sum.length; i++) {
+  [expected, result] = [sum[i], findDigit(i)];
+  console.log(`CASE ${i}`)
+  console.log(`expected: ${expected}`)
+  console.log(`fact: ${result}`);
+  console.log(expected == result);
 }
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(`CASE n === ${i}`);
+//   console.log(findDigit(i));
+// }
 
 
