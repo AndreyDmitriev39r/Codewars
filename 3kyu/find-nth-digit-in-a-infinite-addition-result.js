5bafa3e9a9f2e3d71c2f0e66000879c7d5e3bfa5
Andrey Dmitriev <admit8490@gmail.com>
2023-04-01 18:09:38 +0200

    alternative sequence generation

--- 3kyu/find-nth-digit-in-a-infinite-addition-result.js
+++ 3kyu/find-nth-digit-in-a-infinite-addition-result.js
@@ -10,15 +10,6 @@ const getDigitsFromNatSequence = (n) => {
   return [Number(result[n -1]), Number(result[n])];
 }
 
-// alternative functions to generate sequences -????????
-// note 1
-// for generating result >>> only four digits needed
-// thus, there should be a way to get those without building dynamically either arrays or strings >> stuck on indexing,
-// and stuck on rewriting temp variables properly
-// note 2
-// there is a pattern in squares sequence, which will allow to build squares only using addition, without ** operation
-// !!!REALIZED BELOW - had +perfomance effect, but not enough
-
 const getDigitsFromSquareSequence = (n) => {
   let step = 1;
   let square = 0;
@@ -31,44 +22,139 @@ const getDigitsFromSquareSequence = (n) => {
   return [Number(result[n -1]), Number(result[n])];
 }
 
-// 123456789101121314151617
+// OPTIMIZATION EFFORT >>> goal - avoid building of the whole sequence
 
-const newGetDigitFromNat = (n) => {
-  let result = {
-    leftDigit: 1,
-    leftIndex: 0,
-    rightDigit: 2,
-    rightIndex: 1,
-  }
-  let length = 2;
-  let currNum = 3;
-  let currNumFormatted;
-  while (length < n + 2) {
-    if (currNum < 10) {
-      length++;
-      result.leftDigit++;
-      result.leftIndex++;
-      result.rightIndex++;
-      result.rightDigit++;
-    } else {
-      currNumFormatted = currNum.toString();
-      console.log(currNumFormatted);
-      return;
+const getDigitsFromNatSequence2 = (n) => {
+  
+  let sequenceLength = 0;
+  let currentNumber = 1;
+  let lengthToAdd;
+  let currentNumberAsString;
+  
+  while (sequenceLength <= n + 1) {
+    currentNumberAsString = currentNumber.toString();
+    lengthToAdd = currentNumberAsString.length;
+    if (sequenceLength + lengthToAdd >= n + 1) {
+      //this block should fire WHEN required digit
+      // somewhere in the current Number
+      // console.log(currentNumber);
+      
+      let digit = currentNumberAsString[0]
+      let digitPosInSequence = sequenceLength;
+      let digitPosInNumber = 0;
+      
+      while (digitPosInSequence !== n) {
+        digitPosInNumber++
+        digitPosInSequence++
+        digit = currentNumberAsString[digitPosInNumber];
+      }
+      // after the above loop digit with position n should be
+      // assigned to digit
+      // console.log(digit)
+      
+      // for getting digit on n + 1 position >>>
+      // different steps depending on
+      // do we need to get next number or not
+      
+      nextDigit = digitPosInNumber !== lengthToAdd - 1
+        ? currentNumberAsString[digitPosInNumber + 1]
+        : (currentNumber + 1).toString()[0];
+      
+      return [Number(digit), Number(nextDigit)];
+      
     }
+    
+    currentNumber++;
+    sequenceLength += lengthToAdd;
   }
-  return result;
+  return 'some stuff definitely went wrong if we not return early'
 }
 
-// console.log(newGetDigitFromNat(8)){leftDigit: 9, leftIndex: 8, rightDigit: 10, rightIndex: 9}
+// ~~~~~~test block for getDigitsFromNatSequence2
 
+let natSequence = "";
+for (let i = 1; i < 100; i++) {
+  natSequence += i;
+};
+// console.log(natSequence)
+
+// for n === x >>> results should be [natSequence[x], natSequence[x + 1]]
+
+// UNCOMMENT BELOW
+// for (let n = 0; n < 20; n++) {
+//   console.log(`CASE n === ${n}`);
+//   console.log(`EXPECTED:`);
+//   console.log([Number(natSequence[n]), Number(natSequence[n + 1])])
+//   console.log(`RESULT:`);
+//   console.log(getDigitsFromNatSequence(n));
+//   console.log(`***************`)
+// };
+// UNCOMMENT ABOVE
+
+// ~~~~~~end of test block for getDigitsFromNatSequence2
+
+const getDigitsFromSquareSequence2 = (n) => {
+  let sequenceLength = 0;
+  let currentNumber = 1;
+  let lengthToAdd;
+  let currentNumberAsString;
+  let step = 3;
+
+  while (sequenceLength <= n + 1) {
+    currentNumberAsString = currentNumber.toString();
+    lengthToAdd = currentNumberAsString.length;
+
+    if (sequenceLength + lengthToAdd >= n + 1) {
+      let digit = currentNumberAsString[0]
+      let digitPosInSequence = sequenceLength;
+      let digitPosInNumber = 0;
+      
+      while (digitPosInSequence !== n) {
+        digitPosInNumber++
+        digitPosInSequence++
+        digit = currentNumberAsString[digitPosInNumber];
+      }
+      
+      nextDigit = digitPosInNumber !== lengthToAdd - 1
+        ? currentNumberAsString[digitPosInNumber + 1]
+        : (currentNumber + step).toString()[0];
+
+      return [Number(digit), Number(nextDigit)];
+
+    }
+    currentNumber += step;
+    sequenceLength += lengthToAdd;
+    step += 2;
+  }
+  return 'some stuff definitely went wrong if we not return early';
+}
+
+// ~~~~test block for getDigitsFromSquareSequence2
+
+let sqSequence = "";
+for (let i = 1; i < 30; i++) {
+  sqSequence += i ** 2;  
+}
+// console.log(sqSequence);
+// for n === x >>> results should be [natSequence[x], natSequence[x + 1]]
+
+// UNCOMMENT BELOW
+// for (let n = 0; n < 20; n++) {
+//   console.log(`CASE n === ${n}`);
+//   console.log(`EXPECTED:`);
+//   console.log([Number(sqSequence[n]), Number(sqSequence[n + 1])])
+//   console.log(`RESULT:`);
+//   console.log(getDigitsFromSquareSequence2(n));
+//   console.log(`***************`)
+// };
+// UNCOMMENT ABOVE
+
+
+// ~~~~end of test block for getDigitsFromSquareSequence2
 
 const findDigit = (n) => {  
-  // const [naturals, squares] = [genSequence(n + 1, false), getSquares(n + 1)];
-  // let result = Number(naturals[n]) + Number(squares[n]);
-  // result += Number(naturals[n + 1]) + Number(squares[n + 1]) >= 10 ? 1 : 0;
-  // return result < 10 ? result : result - 10;
-  const [natLeft, natRight] = getDigitsFromNatSequence(n + 1);
-  const [sqLeft, sqRight] = getDigitsFromSquareSequence(n + 1);
+  const [natLeft, natRight] = getDigitsFromNatSequence2(n);
+  const [sqLeft, sqRight] = getDigitsFromSquareSequence2(n);
   let result = natLeft + sqLeft;
   result += natRight + sqRight >= 10 ? 1 : 0;
   return result < 10 ? result : result - 10;
@@ -76,14 +162,14 @@ const findDigit = (n) => {
 
 // test cases
 
-// const sum = '272619325597593231536305887388';
-// let expected; 
-// let result;
-// for (let i = 0; i < sum.length; i++) {
-//   [expected, result] = [sum[i], findDigit(i)];
-//   console.log(`CASE ${i}`)
-//   console.log(`expected: ${expected}`)
-//   console.log(`fact: ${result}`);
-//   console.log(expected == result);
-//   console.log(`*************`);
-// }
+const sum = '272619325597593231536305887388';
+let expected; 
+let result;
+for (let i = 0; i < sum.length; i++) {
+  [expected, result] = [sum[i], findDigit(i)];
+  console.log(`CASE ${i}`)
+  console.log(`expected: ${expected}`)
+  console.log(`fact: ${result}`);
+  console.log(expected == result);
+  console.log(`*************`);
+}
