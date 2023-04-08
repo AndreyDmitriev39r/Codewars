af7f6f8eef5577a971ff532298a70cd2db044e88
Andrey Dmitriev <admit8490@gmail.com>
2023-04-09 01:15:40 +0200

    counting string subsequences - solved

--- 4kyu/counting-strings-subsequences.js
+++ 4kyu/counting-strings-subsequences.js
@@ -1,8 +1,29 @@
 // TASK LINK >>> https://www.codewars.com/kata/52f7892a747862fc9a0009a6
 
+const getLookupCarcass = needle => Array.from(needle).reduce((acc, _, i) => ({...acc, [i]: []}), {});
+
+const needleCharsInHayStack = (needle, haystack) => {
+  const lookup = getLookupCarcass(needle);
+  for (let i = 0; i < needle.length; i++) {
+    for (let j = 0; j < haystack.length; j++) {
+      if (needle[i] === haystack[j]) {
+        i === 0 ? lookup[i].push([j, 1]) : lookup[i].push([j, 0]);
+      }
+    }
+  }
+  return lookup;
+}
+
 const countSubsequences = (needle, haystack) => {
-  // returns the number of subsequences as an integer
-  // write your solution here ... 
+  const lookup = needleCharsInHayStack(needle, haystack);
+  for (let i = 1; i < needle.length; i++) {    
+    for (let char of lookup[i]) {
+      for (let prevChar of lookup[i - 1]) {
+        if (char[0] > prevChar[0]) char[1] += prevChar[1];        
+      }
+    }
+  }
+  return lookup[needle.length - 1].reduce((result, el) => result + el[1], 0);
 };
 
 const tests = [
